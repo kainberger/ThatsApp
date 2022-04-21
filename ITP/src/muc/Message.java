084a46852d707b4af5757aef2aa695141e1a88b8
Florian Kainb(u)erger <94116352+kainBurger@users.noreply.github.com>
2022-04-21 16:29:25 +0200

    x
    x

---
+++ ITP/src/muc/Message.java
@@ -0,0 +1,43 @@
+package muc;
+
+public class Message {
+    private String msg;
+    private User src;
+    private User target;
+
+
+    public Message(String msg, User src, User target) {
+        setMsg(msg);
+        setSrc(src);
+        setTarget(target);
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    private void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public User getSrc() {
+        return src;
+    }
+
+    private void setSrc(User src) {
+        this.src = src;
+    }
+
+    public User getTarget() {
+        return target;
+    }
+
+    private void setTarget(User target) {
+        this.target = target;
+    }
+
+    @Override
+    public String toString() {
+        return msg;
+    }
+}

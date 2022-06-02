e1433662d96fa5e38acb47963e7a1480254388d3
Fellograce <trinh.christian31@gmail.com>
2022-06-02 17:03:24 +0200

    Login und Chat Fenster

--- comChatsV1/src/layout/chat/viewController/ChatC.java
+++ comChatsV1/src/layout/chat/viewController/ChatC.java
@@ -7,6 +7,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -63,11 +65,6 @@ public class ChatC {
     }
 
     @FXML
-    private void test(ActionEvent event) {
-        System.out.println("asdf");
-    }
-
-    @FXML
     private void sendMessage(ActionEvent event) {
         if (!tfMessage.getText().isEmpty()) {
             showMessage(tfMessage.getText());
@@ -79,10 +76,22 @@ public class ChatC {
         Label lbMessage = new Label(message);
         Label lbTest = new Label(message);
 
-        lbMessage.setAlignment(Pos.CENTER_RIGHT);
-        lbTest.setAlignment(Pos.CENTER_LEFT);
+        lbMessage.getStyleClass().add("friend-msg");
+        lbTest.getStyleClass().add("user-msg");
 
-        vbChatBox.getChildren().add(lbMessage);
-        vbChatBox.getChildren().add(lbTest);
+        HBox hbox = new HBox();
+
+        hbox.getChildren().add(lbMessage);
+        hbox.setAlignment(Pos.CENTER_LEFT);
+
+        vbChatBox.getChildren().add(hbox);
+
+
+        HBox hbox1 = new HBox();
+
+        hbox1.getChildren().add(lbTest);
+        hbox1.setAlignment(Pos.CENTER_RIGHT);
+
+        vbChatBox.getChildren().add(hbox1);
     }
 }

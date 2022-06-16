4f0f9890424ec1851ecd75af83fa04eba78f1185
Christian <trinh.christian31@gmail.com>
2022-06-16 12:58:58 +0200

    updated Chat-Fenster

--- comChatsV1/src/layout/chat/viewController/ChatC.java
+++ comChatsV1/src/layout/chat/viewController/ChatC.java
@@ -1,5 +1,6 @@
 package layout.chat.viewController;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,12 +9,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ChatC {
     @FXML
@@ -34,14 +36,18 @@ public class ChatC {
     @FXML
     private TextField tfMessage;
 
+    //Stage global, um leichter das Fenster schließen zu können.
+    // Mit MenuItem ist es schwer, das Stage zu bekommen.
+    private static Stage stage;
+
     public static void show(Stage owner) {
         try {
-            FXMLLoader loader = new FXMLLoader(ChatC.class.getResource("ChatV.fxml"));
+            FXMLLoader loader = new FXMLLoader(ChatC.class.getResource("test.fxml"));
             Parent root = (Parent) loader.load();
 
             ChatC chatC = (ChatC) loader.getController();
 
-            Stage stage = new Stage();
+            stage = new Stage();
 
             Scene scene = new Scene(root);
             stage.setScene(scene);
@@ -56,6 +62,7 @@ public class ChatC {
 
     @FXML
     private void initialize() {
+        //Auto-scroll nach unten
         spChat.needsLayoutProperty().addListener((observable, oldValue, newValue) -> {
             if (!newValue) {
                 spChat.setVvalue(1.0);
@@ -72,25 +79,46 @@ public class ChatC {
     }
 
     private void showMessage(String message) {
+        //Datum und Zeit
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+        Date date = new Date();
+
         Label lbMessage = new Label(message);
         Label lbTest = new Label(message);
+
+        //Datum und Zeit zur Nachricht hinzufügen
+        lbMessage.setText(lbMessage.getText() + "\n\n" + formatter.format(date));
+        lbTest.setText(lbTest.getText() + "\n\n" + formatter.format(date));
 
         lbMessage.getStyleClass().add("friend-msg");
         lbTest.getStyleClass().add("user-msg");
 
+        //Wrap Text
+        lbMessage.setWrapText(true);
+        lbMessage.setMaxWidth(300);
+
+        lbTest.setWrapText(true);
+        lbTest.setMaxWidth(300);
+
+        //Nachrichten auf dem Bildschirm anzeigen
         HBox hbox = new HBox();
 
-        hbox.getChildren().add(lbMessage);
+        hbox.getChildren().addAll(lbMessage);
         hbox.setAlignment(Pos.CENTER_LEFT);
-
-        vbChatBox.getChildren().add(hbox);
-
 
         HBox hbox1 = new HBox();
 
         hbox1.getChildren().add(lbTest);
         hbox1.setAlignment(Pos.CENTER_RIGHT);
 
+        vbChatBox.getChildren().add(hbox);
         vbChatBox.getChildren().add(hbox1);
     }
+
+    @FXML
+    private void logout(ActionEvent event) {
+        stage.close();
+    }
 }
+
+
